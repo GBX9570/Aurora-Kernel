cb907810e6602dee0f58b53440da2f8afa86e71b
GBX9570 <lr.russell@proton.me>
2024-10-22 22:45:57 +0100

    Double Faults + Hardware Interrupts done

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,45 @@
 #![test_runner(cosmix::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use cosmix::println;
+use core::panic::PanicInfo;
+use cosmix::print;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Cosmix is loading...");
+    println!("Loading Cosmix....{}", "!");
 
     cosmix::init();
 
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+		
+    // uncomment line below to trigger a stack overflow
+    // stack_overflow();
+
     #[cfg(test)]
     test_main();
-    loop {}
+
+    println!("Not Crashed Yet");
+    cosmix::hlt_loop();
 }
 
-#[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    init();      // new
-    test_main();
-    loop {}
-}
-
+/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    cosmix::hlt_loop();
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     cosmix::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
